b0cd8ad4d30743e02dd8d5a1950873524dc28fd7
lxfsz <493032412@qq.com>
2020-04-20 12:11:24 +0800

    test

--- test.js
+++ test.js
@@ -4,3 +4,6 @@ console.log('connectioned remote');
 console.log("git hub");
 console.log('confict ok');
 console.log('input by local');
+
+const name = 'Will Robinson';
+console.warn(`Danger ${name}! Danger!`);
